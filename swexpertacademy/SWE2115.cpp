e997cd3e628b9d46f5ec2b249dc7ff5dc183ab1b
has2 <leesh111112@gmail.com>
2017-10-09 11:22:39 +0900

    SWE2115

--- swexpertacademy/SWE2115.cpp
+++ swexpertacademy/SWE2115.cpp
@@ -1,77 +1,57 @@
 //SW Expert
-//ID : 2115 // brute force
-//2017/10/08
+//ID : 2105 // backtracking
+//2017/10/09
 
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
-#include <vector>
 using namespace std;
-int n, m, c, a[11][11], temp, ret;
-vector<int> ans;
-vector<int> rv1, rv2;
-void sol(int y, int x, int cur, int v, int mcost, vector<int> tv) {
-	if (cur == m) {
-		if (v <= c && temp < mcost) {
-			temp = mcost;
-			ans = tv;
-		}
-		return;
-	}
-	vector<int> nv = tv;
-	nv.push_back(a[y][x + cur]);
-	sol(y, x, cur + 1, v + a[y][x + cur], mcost + a[y][x + cur] * a[y][x + cur], nv);
-	sol(y, x, cur + 1, v, mcost, tv);
+int n, m, c, a[11][11];
 
+int getMax(int y, int x) {
+	
+	int maxsq=0;
+	int possible = (1 << m) - 1;
+	for (int mask = 0; mask <= possible; mask++) {
+		int sum = 0, sq = 0;
+		for (int i = 0; i < m; i++) {
+			if ((1 << i) & mask) {
+				sum += a[y][x + i];
+				sq += a[y][x + i] * a[y][x + i];
+			}
+		}
+		if (sum <= c) maxsq = max(sq, maxsq);
+	}
+	return maxsq;
 }
 
 int main() {
 	int tc;
 	scanf("%d", &tc);
 	for (int testcase = 1; testcase <= tc; testcase++) {
-		ret = 0;
 		scanf("%d%d%d", &n, &m, &c);
 
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++)
 				scanf("%d", &a[i][j]);
 
+		int ret = 0;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n - m + 1; j++) {
-				temp = 0;
-				vector<int> tv;
-				sol(i, j, 0, 0, 0, tv);
-				int v1 = temp;
-				vector<int> v1v = ans;
+				
+				int v1 = getMax(i, j);
 
 				int v2;
-				vector<int> v2v;
-				temp = 0;
 				for (int k = j + m; k <= n - m + 1; k++) {
 					if (k + m - 1 > n) break;
-					tv.clear();
-					sol(i, k, 0, 0, 0, tv);
-					v2 = temp;
-					v2v = ans;
-					if (ret < v2 + v1) {
-						ret = v1 + v2;
-						rv1 = v1v;
-						rv2 = v2v;
-					}
+					
+					v2 = getMax(i, k);
+					if (ret < v2 + v1) ret = v1 + v2;		
 				}
 
 				for (int k = i + 1; k <= n; k++) {
 					for (int l = 1; l <= n - m + 1; l++) {
-						tv.clear();
-
-						sol(k, l, 0, 0, 0, tv);
-						v2 = temp;
-						v2v = ans;
-						if (ret < v2 + v1) {
-							ret = v2 + v1;
-							rv1 = v1v;
-							rv2 = v2v;
-						}
+						v2 = getMax(k, l);
+						if (ret < v2 + v1) ret = v2 + v1;
 					}
 				}
 			}
