4a7a069c93503479cb854b0b5312ef17572e9efd
LEE SANG HWA <leesh111112@gmail.com>
2021-01-01 00:42:19 +0900

    update commit hook codeforces 문제 commit에 대해서도 hook을 제공

--- commit_hook.sh
+++ commit_hook.sh
@@ -1,34 +1,88 @@
 #!/bin/sh
-echo -e "LEVEL : \c"
-read LEVEL
 
-echo -e "문제번호 : \c"
-read NUM
+BOJ='BOJ/백준'
+CF='Codeforces'
+echo -e "플랫폼 선택 (1)=boj, (2)=codeforces : \c"
+read PLATFORM
+if [ ${PLATFORM} -eq 1 ]; then
+    PLATFORM=${BOJ}
+elif [ ${PLATFORM} -eq 2 ]; then
+    PLATFORM=${CF}
+else
+    echo "1과 2중 선택해주세요"
+    exit 1  
+fi
 
-echo -e "문제이름 : \c"
-read NAME
+if [ ${PLATFORM} == ${BOJ} ];then
+    echo -e "LEVEL : \c"
+    read LEVEL
 
-echo -e "유형 : \c"
-read TYPE
-# LEVEL=$1
-# NUM=$2
-# NAME=$3
-# TYPE=$4
+    echo -e "문제번호 : \c"
+    read NUM
+
+    echo -e "문제이름 : \c"
+    read NAME
+
+    echo -e "유형 : \c"
+    read TYPE
+
+    TITLE="[${PLATFORM}][${LEVEL}] ${NUM} : ${NAME} (C++)"
+    LINK="https://www.acmicpc.net/problem/${NUM}"
+    GITLINK="https://github.com/has2/Problem-Solving/blob/master/boj-solved.ac/${LEVEL}/${NUM}.cpp"
+
+    echo "PLATFORM:${PLATFORM}, LEVEL:${LEVEL}, NUM:${NUM}, NAME:${NAME}, TYPE:${TYPE}"
+    cp ../solving.cpp ../boj-solved.ac/${LEVEL}/${NUM}.cpp
+else
+    echo -e "ROUND : \c"
+    read ROUND
+
+    echo -e "DIVISION : \c"
+    read DIV
+    LEVEL=Div.${DIV}
+
+    echo -e "문제 알파벳(A,B,C,D...) : \c"
+    read ALPHA
+    ALPHA=${ALPHA^}
+
+    echo -e "문제이름 : \c"
+    read NAME
+
+    echo -e "유형 : \c"
+    read TYPE
+
+    echo -e "링크 : \c"
+    read LINK
+ 
+    TITLE="[${PLATFORM}][Round #${ROUND}][Div.${DIV}] ${ALPHA} : ${NAME} (C++)"
+
+    DIRNAME=codeforces/Round#${ROUND}-Div.${DIV}
+    GITLINK="https://github.com/has2/Problem-Solving/blob/master/${DIRNAME}/${ALPHA}.cpp"
+
+
+    echo "PLATFORM:${PLATFORM}, ROUND:${ROUND}, DIVISION:${DIV}, ALPHA:${ALPHA}, NAME:${NAME}, TYPE:${TYPE}"
+    
+    
+    if [ ! -d ../${DIRNAME} ]; then
+        echo "Directoy ../${DIRNAME} is not exist.. create DIR"
+        mkdir ../${DIRNAME}
+    fi
+    cp ../solving.cpp ../${DIRNAME}/${ALPHA}.cpp
+fi
+
 DATE=$(date +%Y%m%d)
 
-cp ../solving.cpp ../boj-solved.ac/${LEVEL}/${NUM}.cpp
 
 
-echo "LEVEL:${LEVEL}, NUM:${NUM}, NAME:${NAME}, TYPE:${TYPE}"
+
+echo ${TITLE}
+echo ${LINK}
 
 echo "\
 [${DATE}] ${TYPE}
 
-[BOJ/백준][${LEVEL}] ${NUM} : ${NAME} (C++) 
+${TITLE}
 
-https://www.acmicpc.net/problem/${NUM}
-
-
+${LINK}
 
 
 
@@ -39,7 +93,7 @@ https://www.acmicpc.net/problem/${NUM}
 
 
 
-https://github.com/has2/Problem-Solving/blob/master/boj-solved.ac/${LEVEL}/${NUM}.cpp \
+${GITLINK} \
 " > ../.gitmessage.txt
 git add ../.
 git commit
